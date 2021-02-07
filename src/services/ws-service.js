2876e6deaf203c598f19a6c3c8746ec5f823026a
hujanais <botmaker168@gmail.com>
2021-02-07 12:41:27 -0500

    added ping-pong handling

--- src/services/ws-service.js
+++ src/services/ws-service.js
@@ -9,6 +9,7 @@ class WSService {
     this.espWSS;
     this.wssClients = new Map();
     this.subject = new Subject();
+    this.interval;
     console.log('ctor WSService');
   }
 
@@ -24,16 +25,41 @@ class WSService {
       if (id === 'esp') {
         // this is the ESP.
         ws.id = 'esp';
+        ws.isAlive = true;
         this.espWSS = ws;
 
+        console.log('connection');
+
+        // Handle situation when the ESP gets disconnected using ping-pong.
+        // This time, the server will send a ping and waits for a pong within a certain time.
+        this.interval = setInterval(() => {
+          if (!this.espWSS.isAlive) return this.espWSS.terminate();
+          this.espWSS.isAlive = false; // set isAlive flag to false and wait for the pong to reset it.
+          this.espWSS.ping();
+        }, 5000);
+
+        // handle new messages from the ESP and relay the data to all connected clients.
         this.espWSS.on('message', (msg) => {
           this.wssClients.forEach((ws) => {
             ws.send(`${msg}`);
           });
         });
 
+        // The ESP sends a ping and the server returns a pong to satisfy heartbeat on the ESP side.
         this.espWSS.on('ping', () => {
+          console.log('ping');
           this.espWSS.pong(null);
+        });
+
+        // When pong is received, reset the isAlive flag to true.
+        this.espWSS.on('pong', () => {
+          console.log('pong');
+          this.espWSS.isAlive = true;
+        });
+
+        this.espWSS.on('close', () => {
+          console.log('the ESP has disconnected');
+          clearInterval(this.interval);
         });
       } else {
         // when connection arrives, assigned a guid to this client and store this off in a map.
